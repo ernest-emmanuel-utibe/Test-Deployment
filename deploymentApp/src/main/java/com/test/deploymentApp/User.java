89ff5ea6be40aa897d81d25ba7b3cf71ede1dcdc
ernest-emmanuel-utibe <em123nest@gmail.com>
2024-06-08 23:05:18 +0100

    feat: Fixed Bugs

---
+++ deploymentApp/src/main/java/com/test/deploymentApp/User.java
@@ -0,0 +1,36 @@
+package com.test.deploymentApp;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+}
